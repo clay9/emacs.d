83d4eac8db1f500c5f5e6cf1a729469fb41d642e
wcq <wcq@gamil.com>
2024-01-18 15:20:17 +0800

    script

--- script/export-org-to-md.el
+++ script/export-org-to-md.el
@@ -21,7 +21,7 @@
 ;;(setq org-export-with-author nil)
 ;; no confirm eval
 (setq org-confirm-babel-evaluate nil)
-;; line breaks when exporting -- TODO not work
+;; line breaks when exporting
 (setq org-export-preserve-breaks t)
 ;; support html5
 (setq org-html-html5-fancy t)
@@ -30,9 +30,8 @@
 ;; rm blank-lines
 (setq org-ascii-headline-spacing nil)
 ;; rm blank-lines -- TODO test
-(setq org-list-two-spaces-after-bullet-regexp nil)
-;; rm blank-lines
-(setq org-blank-before-new-entry '((heading . nil) (plain-list-item . nil)))
+;;(setq org-list-two-spaces-after-bullet-regexp nil)
+
 
 ;;; ox-hugo
 ;; use org-mode-generate-toc
@@ -45,8 +44,25 @@
   (setq org-hugo-base-dir root_dir)
   (dolist (file (directory-files-recursively root_dir ".org"))
     (with-current-buffer (find-file-noselect file)
-      (org-hugo-export-to-md))))
+      (org-hugo-export-to-md)))
+  (my/hugo-rm-br root_dir))
 
+;; fun: rm <br/> after plain-list
+(defun my/hugo-rm-br(root_dir)
+  (dolist (file (directory-files-recursively root_dir ".md"))
+    (with-current-buffer (find-file-noselect file)
+      (save-excursion
+        (save-restriction
+          (beginning-of-buffer)
+          (while (not (eobp))
+            (let* ((info (thing-at-point 'line))
+                   (m1 (string-match "^[ \t]*\\(\\(?:[-+*]\\|\\(?:[0-9]+\\|[A-Za-z]\\)[.)]\\)\\(?:[ \t]*+\\|$\\)\\)" info))
+                   (m2 (string-match "<br/>[ \t]*?$" info)))
+              (when (and m1 m2)
+                (beginning-of-line)
+                (delete-region (point) (line-end-position))
+                (insert (substring info 0 m2))))
+            (forward-line 1)))))))
 
 ;; org-files 输出为 md-files
 ;; github action中, 该fun 会在./hugo中执行. 因此这里传递.即可
