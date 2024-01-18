59619c377e92018262e74ad501c7c8213ad5b752
wcq <wcq@gamil.com>
2024-01-18 19:22:29 +0800

    yasnippet

--- lisp/init-text-snippet.el
+++ lisp/init-text-snippet.el
@@ -5,6 +5,18 @@
 (use-package yasnippet
   :diminish yas-minor-mode
   :config
+  ;;; funs for yasnippet template
+  (defun yas/hugo-get-bundle()
+    (let* ((file-path (expand-file-name (buffer-file-name)))
+           (file-dir (file-name-directory file-path))
+           (index (string-match "org/" file-dir))
+           (rpath (substring file-dir (+ index 4) -1)))
+      rpath))
+  (defun yas/hugo-get-bundle-name()
+    (let* ((file-path (expand-file-name (buffer-file-name)))
+           (file-name (file-name-sans-extension (file-name-nondirectory file-path))))
+      file-name))
+  :config
   (setq yas-snippet-dirs (list (expand-file-name ".yasnippet" user-emacs-directory)))
   ;; start must after settings, or settings will not work
   (yas-global-mode t))
