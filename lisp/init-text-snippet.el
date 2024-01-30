f5a35cb00851b790d5f81b1702fa8113a5cadcba
wcq <wcq@gamil.com>
2024-01-30 10:25:23 +0800

    fix hugo-bundle

--- lisp/init-text-snippet.el
+++ lisp/init-text-snippet.el
@@ -10,12 +10,23 @@
     (let* ((file-path (expand-file-name (buffer-file-name)))
            (file-dir (file-name-directory file-path))
            (index (string-match "org/" file-dir))
-           (rpath (substring file-dir (+ index 4) -1)))
-      rpath))
+           (rpath (substring file-dir (+ index 4) -1))
+           (file-name (file-name-sans-extension (file-name-nondirectory file-path))))
+      (if (string= file-name "_index")
+          rpath
+        (concat rpath "/" file-name))))
   (defun yas/hugo-get-bundle-name()
     (let* ((file-path (expand-file-name (buffer-file-name)))
            (file-name (file-name-sans-extension (file-name-nondirectory file-path))))
-      file-name))
+      (if (string= file-name "_index")
+          "_index"
+        "index")))
+  (defun yas/hugo-get-collapse-section()
+    (let* ((file-path (expand-file-name (buffer-file-name)))
+           (file-name (file-name-sans-extension (file-name-nondirectory file-path))))
+      (if (string= file-name "_index")
+          "true"
+        "false")))
   :config
   (setq yas-snippet-dirs (list (expand-file-name ".yasnippet" user-emacs-directory)))
   ;; start must after settings, or settings will not work
