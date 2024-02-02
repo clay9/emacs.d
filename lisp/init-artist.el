1e2835fcf95d5c093605cbe0b3d6a0310dc200c4
wcq <wcq@gamil.com>
2024-02-02 19:54:44 +0800

    artist

--- lisp/init-artist.el
+++ lisp/init-artist.el
@@ -27,6 +27,44 @@
         tab-width 4
         indent-tabs-mode nil)
 
+  
+  (defun artist/split-string-by-length (string max-length)
+    (let* ((ss (string-split string))
+           inc_temp
+           temp
+           ans)
+      (dolist (val ss)
+        (setq inc_temp (if temp (concat " " val) val))
+        (if (< (+ (length temp) (length inc_temp)) max-length)
+            (setq temp (concat temp inc_temp))
+          (setq ans (append ans (cons temp nil)))
+          (setq temp val)))
+      (when temp (setq ans (append ans (cons temp nil))))
+      (delq nil ans)))
+  (defun artist/draw-rectangle (&optional anchor pos size text text-align)
+    (artist-select-op-rectangle)
+    (let* ((size (cons (1- (car size)) (1- (cdr size))))
+           (pos (if pos pos (cons (artist-current-column) (artist-current-line))))
+           (pos (cond ((= anchor 1) pos)
+	                 ((= anchor 2) (cons (- (car pos) (/ (car size) 2)) (cdr pos)))
+	                 ((= anchor 3) (cons (car pos) (- (cdr pos) (/ (cdr size) 2))))))
+           (text-align (if text-align text-align 0))
+           (ss (artist/split-string-by-length text (- (car size) text-align)))
+           (index 0))
+      ;; insert text      
+      (while (< index (min (length ss) (1- (cdr size))))
+        (artist-move-to-xy (+ (car pos) 1 text-align) (+ (cdr pos) 1 index))
+        (insert (nth index ss))
+        (setq index (1+ index)))
+      ;; make sure enough space to draw
+      (artist-move-to-xy (+ (car pos) (car size)) (+ (cdr pos) (cdr size)))
+      ;; insert rectangle
+      (artist-move-to-xy (car pos) (cdr pos))
+      (artist-key-set-point)
+      (artist-forward-char (car size))
+      (artist-next-line (cdr size))
+      (artist-key-set-point)
+      (artist-move-to-xy (car pos) (cdr pos))))
 
   (defun my-artist-mode-draw-line (&optional col_row length)
     (interactive "Ncol(1), row(2): \nNlength: ")
@@ -45,24 +83,11 @@
     (artist-select-op-line)
     (artist-key-set-point))
 
-  (defun my-artist-mode-draw-rectangle (&optional pos col row)
-    "Used by hydra-artist-mode"
-    (interactive "Ntop-left(1), top-center(2), left-center(3): \nNcol: \nNrow:")
-    (artist-select-op-rectangle)
-    (let ((point_col (artist-current-column))
-	  (point_row (artist-current-line))
-	  (col (- col 1))
-	  (row (- row 1)))
-      (cond ((= pos 1) ); do nothing
-	    ((= pos 2)
-	     (artist-move-to-xy (- point_col (/ col 2)) point_row))
-	    ((= pos 3)
-	     (artist-move-to-xy point_col (- point_row (/ row 2)))))
-      (artist-key-set-point)
-      (artist-forward-char col)
-      (artist-next-line row)
-      (artist-key-set-point)
-      (artist-move-to-xy point_col point_row)))
+  (defun my-artist-mode-draw-rectangle (&optional anchor col row text)
+    (interactive "Ntop-left(1), top-center(2), left-center(3): \nNcol: \nNrow: \nsText: \n")
+    (let ((pos (cons (artist-current-column) (artist-current-line)))
+          (size (cons col row)))
+      (artist/draw-rectangle anchor pos size text 1)))
 
   ;; TODO 该函数没有经过测试, 好像真实的半径应该是col-1, row-1才对
   (defun my-artist-mode-draw-ellipse (&optional col row)
