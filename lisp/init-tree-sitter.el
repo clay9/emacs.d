09363e488269108eefbf8b365f1994f703383ed1
clay9 <15178234+clay9@users.noreply.github.com>
2024-04-25 14:36:56 +0800

    Update init-tree-sitter.el

--- lisp/init-tree-sitter.el
+++ lisp/init-tree-sitter.el
@@ -8,5 +8,8 @@
 (when (eq system-type 'darwin)
   (setq treesit-extra-load-path (list (concat user-emacs-directory "tree-sitter/" "macos"))))
 
+(when (eq system-type 'gnu/linux)
+  (setq treesit-extra-load-path (list (concat user-emacs-directory "tree-sitter/" "linux"))))
+
 (provide 'init-tree-sitter)
 ;;; init-tree-sitter.el ends here
