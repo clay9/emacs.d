ed5fe194c125c78acdb0af0331b9ad4c82944980
clay <3@gmail.com>
2024-04-30 16:35:51 +0800

    add tty keys
    C-<return>

--- lisp/init-tty-keys.el
+++ lisp/init-tty-keys.el
@@ -2,6 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
+(define-key input-decode-map (quote [27 91 51 59 97 126]) (kbd "C-<return>"))
 (define-key input-decode-map (quote [27 91 51 59 98 126]) (kbd "C--"))
 (define-key input-decode-map (quote [27 91 51 59 99 126]) (kbd "C-="))
 (define-key input-decode-map (quote [27 91 51 59 100 126]) (kbd "C-<backspace>"))
