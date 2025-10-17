67e6f571e70cbb714d7da0e4f734f3cccf4c4561
clay <clay@claydeMac-mini.local>
2025-10-18 06:50:54 +0800

    t

--- script/init-for-script.el
+++ script/init-for-script.el
@@ -6,7 +6,6 @@
 ;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
 (setq debug-on-error t)
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "script" user-emacs-directory))
 
 (require 'init-package)
